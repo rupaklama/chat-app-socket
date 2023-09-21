43df22bede11431c655976c6f747667673d1e0a0
Rupak Lama <rupaklamadev@gmail.com>
2023-09-21 18:48:24 -0400

    broadcast event to everyone

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,12 +6,25 @@ const socket = io();
 
 // Receiving particular Server Event & args in the callback here
 // note: the argument name can be anything here
-socket.on("countUpdated", count => {
-  console.log("The count has been updated!", count);
-});
+// socket.on("countUpdated", count => {
+//   console.log("The count has been updated!", count);
+// });
 
 // Emit an Event from the Client to send data to the server
-document.querySelector("#increment").addEventListener("click", () => {
-  // no need to send any data since the server knows the current count & adds one to it
-  socket.emit("increment");
+// document.querySelector("#increment").addEventListener("click", () => {
+//   // no need to send any data since the server knows the current count & adds one to it
+//   socket.emit("increment");
+// });
+
+// handling events
+// note: on callback arg, client has access to data sent by server
+socket.on("message", msg => {
+  console.log(msg);
+});
+
+document.querySelector("#message-form").addEventListener("submit", e => {
+  e.preventDefault();
+
+  const userMsg = e.target.elements.messageBox.value;
+  socket.emit("clientMessage", userMsg);
 });
--- server.js
+++ server.js
@@ -19,7 +19,8 @@ const port = process.env.port || 2000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-let count = 0;
+// let count = 0;
+const message = "welcome to Chatty!";
 
 // socket param is an object that contains information about the current connection
 io.on("connection", socket => {
@@ -27,18 +28,35 @@ io.on("connection", socket => {
 
   // socket.emit() is to send Event from our server with some data to the Client
   // note: need to setup our own 'custom' events to feed the need of our application
-  socket.emit("countUpdated", count);
+  // socket.emit("countUpdated", count);
   // note: the second arg value in the event above is accessible in the client on the callback function
 
   // note: listening an event in the client
-  socket.on("increment", () => {
-    count++;
+  // socket.on("increment", () => {
+  //   count++;
 
-    // notify only particular connection in the client with updated data
-    // socket.emit("countUpdated", count);
+  // notify only particular connection in the client with updated data
+  // socket.emit("countUpdated", count);
 
-    // note: notify many connected connections at once
-    io.emit("countUpdated", count);
+  // note: notify many connected connections at once
+  //   io.emit("countUpdated", count);
+  // });
+
+  // note: Emit ONLY to the PARTICULAR CONNECTION
+  socket.emit("message", message);
+
+  // note: Emit to EVERYONE but not to the current particular connection
+  socket.broadcast.emit("message", "A new user has joined!");
+
+  // note: on callback arg, server has access to data sent by client
+  socket.on("clientMessage", msg => {
+    //  note: io.emit to send/emit to EVERYONE
+    io.emit("message", msg);
+  });
+
+  // built in event
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
   });
 });
 
